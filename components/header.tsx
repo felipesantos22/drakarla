e94360e0cbc5713ef48e3327e4c4d7c7e74b46ff
felipesantos22 <felipevs2018@gmail.com>
2026-03-30 13:53:31 -0300

    insert header

---
+++ components/header.tsx
@@ -0,0 +1,57 @@
+import Image from "next/image";
+
+export default function Header() {
+  return (
+    <header className="flex items-center justify-between fixed top-0 left-0 w-full p-4 shadow-lg z-10">
+      <Image
+        src="/img/logo.png"
+        width={100}
+        height={100}
+        alt="Picture of the author"
+      />
+      <nav
+        className={`absolute top-full left-0 w-full md:static md:block md:w-auto`}
+      >
+        <ul className="flex flex-col md:flex-row md:space-x-4 p-4 md:p-0">
+          <li>
+            <a
+              href="/"
+              className="block py-2 text-white hover:text-amber-500"
+            >
+              HOME
+            </a>
+          </li>
+          <li>
+            <a
+              href="/pacotes"
+              className="block py-2 text-white hover:text-amber-500"
+            >
+              PROCEDIMENTOS
+            </a>
+          </li>
+          <li>
+            <a
+              href="/seguro"
+              className="block py-2 text-white hover:text-amber-500"
+            >
+              ABOUT
+            </a>
+          </li>
+          <li>
+            <a
+              href="/docs"
+              className="block py-2 text-white hover:text-amber-500"
+            >
+              CONTATO
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <div>
+        <button className="text-white px-4 py-2 rounded-full shadow-2xl border-2">
+          RESERVA
+        </button>
+      </div>
+    </header>
+  );
+}
